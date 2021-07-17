4e91b53bc68d076a4adf223ee3e387d30616238e
Solidshots <seanbrent5@gmail.com>
2021-07-17 12:34:51 -0400

    type defs

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -3,8 +3,46 @@ const {gql} = require('apollo-server-express');
 
 //define our gql typedefs
 const typeDefs = gql`
-    type Query {
-        helloWorld: String
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        title: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    input BookContent {
+        bookId: ID
+        title: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+    }
+
+    type Query{
+        me: User
+    }
+
+    type Mutation{
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(content: BookContent!): User
+        removeBook(bookId: ID!): User
     }
 `;
 
