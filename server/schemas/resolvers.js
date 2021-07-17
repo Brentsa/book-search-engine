0907f5141e6f7c3905a06193f3f2adb04e33f4fe
Solidshots <seanbrent5@gmail.com>
2021-07-17 13:30:16 -0400

    login and adduser resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,43 @@
+const {User} = require('../models');
+const {AuthenticationError} = require('apollo-server-express');
+const {signToken} = require('../utils/auth');
+
 //define resolvers to instruct apollo server how to fetch data
 const resolvers = {
     Query: {
-        helloWorld: () => {
-          return 'Hello world!';
+        me: async () => {
+             
         }
-      }
+    },
+    Mutation: {
+        login: async (_, args) => {
+            //check for a user and throw an error if not found
+            const user = await User.findOne({email: args.email});
+            if(!user) throw new AuthenticationError('Incorrect credentials used.');
+            
+            //verify the user password and throw an error if it doesn't match
+            const verified = await user.isCorrectPassword(args.password);
+            if(!verified) throw new AuthenticationError('Incorrect credentials used.');
+
+            //if the user is verified then create a token for them
+            const token = signToken(user);
+            return {token, user};
+
+        },
+        addUser: async (_, args) => {
+            //create a user in the mongoose database based on the arguments
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return {token, user};
+        },
+        saveBook: async (_, {content}) => {
+
+        },
+        removeBook: async (_, {bookId}) => {
+
+        }
+    }
 };
 
 module.exports = resolvers;
